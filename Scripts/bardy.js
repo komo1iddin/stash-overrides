6a41e99def3c7e17ea64647819cfee08cbc68647
komo1iddin <45395275+komo1iddin@users.noreply.github.com>
2023-09-29 22:59:43 +0500

    Update bardy.js

--- Scripts/bardy.js
+++ Scripts/bardy.js
@@ -1,37 +1,27 @@
-var objc = JSON.parse($response.body);
-    objc = {
-  "request_date_ms": 1695805892915,
-  "request_date": "2023-09-27T09:11:32Z",
-  "subscriber": {
-    "non_subscriptions": {},
-    "first_seen": "2023-09-10T16:38:36Z",
-    "original_application_version": "202308101548",
-    "other_purchases": {},
-    "management_url": null,
-    "subscriptions": {
-      "com.am.gpt3.monthly": {
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj.subscriber.entitlements = {
+      "Pro":{      
+              "product_identifier": "com.am.gpt3.monthly",
+              "purchase_date":"2023-09-10T15:58:24Z",
+              "expires_date":"2099-09-13T16:39:22Z",
+      }
+  },
+  
+obj.subscriber.subscriptions = {
+      "com.am.gpt3.monthly":{
         "original_purchase_date": "2023-09-10T15:58:25Z",
         "expires_date": "2099-09-13T16:39:22Z",
+        "is_sandbox": false,
         "refunded_at": null,
         "unsubscribe_detected_at": null,
+        "period_type": "normal",
         "purchase_date": "2023-09-10T15:58:24Z",
-        "billing_issues_detected_at": null,
         "ownership_type": "PURCHASED",
         "store": "app_store",
-        "store_transaction_id": "90001712911472"
       }
-    },
-    "entitlements": {
-      "Pro": {
-        "grace_period_expires_date": null,
-        "purchase_date": "2023-09-10T15:58:24Z",
-        "product_identifier": "com.am.gpt3.monthly",
-        "expires_date": "2099-09-13T16:39:22Z"
-      }
-    },
-    "original_purchase_date": "2023-08-19T02:46:25Z",
-    "original_app_user_id": "$RCAnonymousID:f120e2dfdb994e428d73047a406f2bde",
-    "last_seen": "2023-09-27T05:52:38Z"
   }
-}
-$done({body : JSON.stringify(objc)});
+
+body = JSON.stringify(obj);
+$done({body});
