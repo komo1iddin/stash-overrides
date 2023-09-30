5621686506626cef9a6a39dca1d0fdc50899b3ae
komo1iddin <45395275+komo1iddin@users.noreply.github.com>
2023-09-30 06:43:48 +0500

    update bardy.js

--- Scripts/bardy.js
+++ Scripts/bardy.js
@@ -1,14 +1,6 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.subscriber.entitlements = {
-      "Pro":{
-              "product_identifier": "com.am.gpt3.monthly",
-              "purchase_date":"2023-09-10T15:58:24Z",
-              "expires_date":"2099-09-13T16:39:22Z",
-      }
-  },
-  
 obj.subscriber.subscriptions = {
       "com.am.gpt3.monthly":{
         "original_purchase_date": "2023-09-10T15:58:25Z",
@@ -21,7 +13,15 @@ obj.subscriber.subscriptions = {
         "ownership_type": "PURCHASED",
         "store": "app_store",
       }
-  }
+  },
+
+  obj.subscriber.entitlements = {
+    "Pro":{
+            "product_identifier": "com.am.gpt3.monthly",
+            "purchase_date":"2023-09-10T15:58:24Z",
+            "expires_date":"2099-09-13T16:39:22Z",
+    }
+}
 
 body = JSON.stringify(obj);
 $done({body});
