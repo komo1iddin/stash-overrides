484ba78acd3c72024045f9c7a1125e6846545e0b
komo1iddin <45395275+komo1iddin@users.noreply.github.com>
2023-10-01 22:04:28 +0500

    Update universal_revenuecat.js

--- Scripts/universal_revenuecat.js
+++ Scripts/universal_revenuecat.js
@@ -1,6 +1,10 @@
 var body = $response.body;
 
-// Replace "expires_date" with the new value using regular expression
-body = body.replace(/"expires_date"\s*:\s*"[^"]+"/g, '"expires_date": "2099-12-31T23:59:59Z"');
+// Replace multiple fields in JSON using regular expressions
+body = body.replace(/("expires_date"\s*:\s*")[^"]+"/g, '$1"2099-12-31T23:59:59Z"');
+body = body.replace(/("is_sandbox"\s*:\s*)[^,]+/g, '$1false');
+body = body.replace(/("refunded_at"\s*:\s*)[^,]+/g, '$1null');
+body = body.replace(/("unsubscribe_detected_at"\s*:\s*)[^,]+/g, '$1null');
+body = body.replace(/("period_type"\s*:\s*")[^"]+"/g, '$1"normal"');
 
 $done({ body });
